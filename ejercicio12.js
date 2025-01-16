32f7eb730b01ea4b20d03583c9e86a24112cc10d
alber6 <143524073+alber6@users.noreply.github.com>
2025-01-16 23:17:26 +0100

    modified ejercicio12

--- ejercicio12.js
+++ ejercicio12.js
@@ -1,8 +1,3 @@
-// Valores únicos: Crea una función que reciba por parámetro un array y 
-// compruebe si existen elementos duplicados, 
-// en caso que existan los elimina para retornar un array sin los elementos duplicados.
-// Pista: puedes generar un nuevo array y devolverlo.
-
 const duplicates = [
     'sushi',
     'pizza',
